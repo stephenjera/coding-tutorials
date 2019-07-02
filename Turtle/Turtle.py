ea3c12c725f75680bd4857d1933ae2eedb014b74
blaze <stephen.411@hotmail.co.uk>
2019-07-02 09:39:04 +0100

    Added gitignore file

---
+++ Turtle/Turtle.py
@@ -0,0 +1,8 @@
+import turtle
+
+window = turtle.Screen()  # New window for turtle
+window.bgcolor("light green")  # Background colour
+window.title("Turtle")
+ed = turtle.Turtle()  # New turtle name Edward
+ed.forward(300)
+ed.done()
