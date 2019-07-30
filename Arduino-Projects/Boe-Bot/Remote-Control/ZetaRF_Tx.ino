d1abed6c56bc09e29a18d37af94f979d1c339416
blaze <stephen.411@hotmail.co.uk>
2019-07-30 18:20:28 +0100

    Setting up VS code.

--- Arduino-Projects/Boe-Bot/Remote-Control/ZetaRF_Tx.ino
+++ Arduino-Projects/Boe-Bot/Remote-Control/ZetaRF_Tx.ino
@@ -10,11 +10,10 @@
 // Zeta modules transmit messages using fixed size packets, define here the max size you want to use
 #define ZETARF_PACKET_LENGTH 16
 
-ZetaRF zeta(10, 9, 8);  // Pins: SPI CS, Shutdown, IRQ
+ZetaRF zeta(10, 9, 8); // Pins: SPI CS, Shutdown, IRQ
 
 int data[ZETARF_PACKET_LENGTH] = {0};
 bool transmitting = false;
-
 
 void setup()
 {
@@ -22,12 +21,12 @@ void setup()
   pinMode(A1, INPUT); // X - axis
   Serial.begin(115200);
   delay(1000);
-  
+
   Serial.println("Starting Zeta TxRx...");
 
   // Initialize Zeta module, specifing channel and packet size
   zeta.begin(2, ZETARF_PACKET_LENGTH);
-  
+
   // Set module in receive mode
   zeta.startListening();
 
@@ -39,7 +38,7 @@ uint8_t joystick[3];
 void loop()
 {
   // Send any data received from serial
- /* if (Serial.available() && !transmitting) {
+  /* if (Serial.available() && !transmitting) {
     int s = Serial.readBytes(data, ZETARF_PACKET_LENGTH);
 
     // Pad with zeros
@@ -49,27 +48,28 @@ void loop()
   joystick[0] = map(analogRead(A0), 0, 1023, 0, 255);
   joystick[1] = map(analogRead(A1), 0, 1023, 0, 255);
   joystick[2] = 100;
-    int input0 = joystick[0];
-    int input1 = joystick[1];
-    int input2 = joystick[2];
-    Serial.print("Sending >");
-    Serial.print(input0);
-    Serial.print(input1);
-    Serial.print(input2);
-    Serial.print("<\n");
+  int input0 = joystick[0];
+  int input1 = joystick[1];
+  int input2 = joystick[2];
+  Serial.print("Sending >");
+  Serial.print(input0);
+  Serial.print(input1);
+  Serial.print(input2);
+  Serial.print("<\n");
 
-    // Send buffer
-    transmitting = true;  // Only one at a time!
-    zeta.sendPacket(2,joystick,ZETARF_PACKET_LENGTH);  // Use channel set with begin()
-    // Module will automatically return to listening mode
- // }
+  // Send buffer
+  transmitting = true;                                // Only one at a time!
+  zeta.sendPacket(2, joystick, ZETARF_PACKET_LENGTH); // Use channel set with begin()
+  // Module will automatically return to listening mode
+  // }
 
   // Check if message was transmitted successfully
-  if (zeta.checkTransmitted()) {
+  if (zeta.checkTransmitted())
+  {
     transmitting = false;
     Serial.println("msg transmitted");
-  }//*/
-/*
+  } //*/
+    /*
   // Check incoming messages and print
   if (zeta.checkReceived()) {
     Serial.print("> ");
@@ -77,6 +77,6 @@ void loop()
     Serial.write(data, ZETARF_PACKET_LENGTH);
     Serial.print('\n');
   }//*/
-  
+
   delay(10);
 }
