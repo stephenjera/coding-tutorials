d74173d8d00449c8e75864303982b142db1b3776
blaze <stephen.411@hotmail.co.uk>
2019-11-12 10:39:28 +0000

    + Lesson 5 of tutorials.

---
+++ Teaching-C/lesson05_functions/Project1/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+//#include <math.h>
+
+constexpr auto sqrd = 2;
+
+// Function prototypes 
+int sum(int a, int b);
+
+// Create a function that finds the hypotenuse of a right angle triangle
+float hypotenuse(float len1, float len2);
+
+ void main() { 
+
+	float result = hypotenuse(3, 4);
+
+	std::cout << "The hypotenuse is: " << result << std::endl;
+
+}
+
+// Function definitions
+ int sum(int a, int b) {
+	 return a + b;
+ }
+
+ float hypotenuse(float len1, float len2) {
+	 //return sqrt((len1 * len1) + (len2 * len2));
+	 return sqrt(pow(3, sqrd) + pow(4 ,sqrd));
+ }
+
