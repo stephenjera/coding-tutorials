2dc63fafea18a9df9d531fb38f57d572cbcfdba2
blaze <stephen.411@hotmail.co.uk>
2019-12-08 07:53:16 +0000

    Completed interrupt code.

--- Digital-Instrumentation/WSC055 Lab Code Pack/Lab 1 2019/main.c
+++ Digital-Instrumentation/WSC055 Lab Code Pack/Lab 1 2019/main.c
@@ -12,17 +12,22 @@
 
 
 void delay(int a); // prototype for delay function
+int counter = 0x0000; // Global variable
 
 int main(void)
 {
-	// Enable clock on GPIO port E
-	RCC->AHBENR |= RCC_AHBENR_GPIOEEN;
+	
+	/* Setting up timer*/
 	RCC->APB1ENR |= RCC_APB1ENR_TIM3EN; // Directs clock pules to timer
 	TIM3->PSC = 100; // prescalor value in Timer ‘x’ as 100
-  TIM3->ARR = 1000; // Auto-Reset Register of Timer ‘x’ set to 1000 counts
-	TIM3->CR1 |= TIM_CR1_CEN; // Timer enable
+	TIM3->ARR = 1000; // Auto-Reset Register of Timer ‘x’ set to 1000 counts
+	TIM3->CR1 |= TIM_CR1_CEN; // //Set Timer Control Register to start timer
 	TIM3->DIER |= TIM_DIER_UIE; // Set DIER register to watch out for an ‘Update’ Interrupt Enable (UIE) – or 0x00000001
-	NVIC_EnableIRQ(TIM3_IRQn);  //  Enable Timer ‘x’ interrupt request in NVIC
+	
+	RCC->AHBENR |= RCC_AHBENR_GPIOEEN; // Enable clock on GPIO port E
+	
+	NVIC_EnableIRQ(TIM3_IRQn);  //  Enable Timer ‘3’ interrupt request in NVIC
+	
 	// GPIOE is a structure defined in stm32f303xc.h file
 	// Define settings for each output pin using GPIOE structure
 	GPIOE->MODER |= 0x55550000; // Set mode of each pin in port E (doesn't work without this)
@@ -30,26 +35,26 @@ int main(void)
 	GPIOE->PUPDR &= ~(0x55550000); // Set Pull up/Pull down resistor configuration for Port E (still wroks when set to zero?)
 	
 	//int counter = 0x0000;
-	/*// Main programme loop - make LED 4 (attached to pin PE.0) turn on and off	
+	// Main programme loop - make LED 4 (attached to pin PE.0) turn on and off	
 	while (1)
   {
 		//uint16_t counter = 0x0000;
-		
-		
-		GPIOE->BSRRL =  counter << 8; // Bit set register (BSRRL) L = set low
+			
+		/*GPIOE->BSRRL =  counter << 8; // Bit set register (BSRRL) L = set low
 		delay(1*1000000); // On time  // Can't get accurate time with this method
 		GPIOE->BSRRH =  counter << 8; 
 		counter++;
-		//delay(1*1000000); // Off time
+		//delay(1*1000000); // Off time*/
 		
-	}*/
+	}
 
-}
+};
 
 // Interrupt handler
-void TIMx_IRQHandler()
+void TIM3_IRQHandler()
 {
-	int counter = 0x0000;
+	//int counter = 0x0000;
+	void delay(int a); // prototype for delay function
 	if ((TIM3->SR & TIM_SR_UIF) !=0) // Check interrupt source is from the ‘Update’ interrupt flag
 	{
 		//...INTERRUPT ACTION HERE
@@ -62,9 +67,11 @@ void TIMx_IRQHandler()
 		//delay(1*1000000); // Off time
 	}
 	TIM3->SR &= ~TIM_SR_UIF; // Reset ‘Update’ interrupt flag in the SR register
+	
 }
 
-/*// Delay function to occupy processor
+// Delay function to occupy processor
+
 void delay (int a)
 {
     volatile int i,j;
@@ -75,4 +82,4 @@ void delay (int a)
     }
 
     return;
-}*/
+}
