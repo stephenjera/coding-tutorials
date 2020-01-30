988791240569dedbf64db743163462061d1723bc
blaze21777 <stephen.411@hotmail.co.uk>
2020-01-30 15:05:00 +0000

    Updated pill dispenser.

--- Arduino-Projects/Pill-Dispenser/Pill_Dispenser_jan22a/Pill_Dispenser_jan22a.ino
+++ Arduino-Projects/Pill-Dispenser/Pill_Dispenser_jan22a/Pill_Dispenser_jan22a.ino
@@ -7,7 +7,7 @@ File myFile;
 DateTime now;
 RTC_DS3231 rtc;
 const int buttonPin = 2;     // the number of the pushbutton pin
-const int ledPin =  13;      // the number of the LED pin
+const int ledPin = 13;      // the number of the LED pin
 
 int buttonState = 0;         // variable for reading the pushbutton status
 
@@ -19,60 +19,60 @@ void setup() {
   // initialize the pushbutton pin as an input:
   pinMode(buttonPin, INPUT);
 
-   if (! rtc.begin()) 
+  if (!rtc.begin())
   {
     Serial.println("Couldn't find RTC Module");
     while (1);
   }
 
- /* if (rtc.lostPower()) 
-  {
-    Serial.println("RTC lost power, lets set the time!");
-    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-  }
-  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));*/
+  /* if (rtc.lostPower())
+   {
+     Serial.println("RTC lost power, lets set the time!");
+     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+   }
+   rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));*/
 
   Serial.print("Initializing SD card...");
   // On the Ethernet Shield, CS is pin 4. It's set as an output by default.
   // Note that even if it's not used as the CS pin, the hardware SS pin 
   // (10 on most Arduino boards, 53 on the Mega) must be left as an output 
   // or the SD library functions will not work. 
-   pinMode(10, OUTPUT);
+  pinMode(10, OUTPUT);
 
-   if (!SD.begin(4)) {
+  if (!SD.begin(4)) {
     Serial.println("initialization failed!");
     return;
   }
   Serial.println("initialization done.");
 
-  myFile = SD.open("test.txt", FILE_WRITE);
-  
-  // if the file opened okay, write to it:
-  if (myFile) {
-    Serial.print("Writing to test.txt...");
-    myFile.println("30");
-    myFile.close(); // close the file:
-    Serial.println("done.");
-  } else {
-    // if the file didn't open, print an error:
-    Serial.println("error opening test.txt");
-  }
-
-   // re-open the file for reading:
-  myFile = SD.open("test.txt");
-  if (myFile) {
-    Serial.println("test.txt:");
-    
-    // read from the file until there's nothing else in it:
-    while (myFile.available()) {
-      Serial.write(myFile.read());
-  }
-    // close the file:
-    myFile.close();
-  } else {
-    // if the file didn't open, print an error:
-    Serial.println("error opening test.txt");
-  }
+  //  myFile = SD.open("test.txt", FILE_WRITE);
+  //  
+  //  // if the file opened okay, write to it:
+  //  if (myFile) {
+  //    Serial.print("Writing to test.txt...");
+  //    myFile.println("30");
+  //    myFile.close(); // close the file:
+  //    Serial.println("done.");
+  //  } else {
+  //    // if the file didn't open, print an error:
+  //    Serial.println("error opening test.txt");
+  //  }
+  //
+  //   // re-open the file for reading:
+  //  myFile = SD.open("test.txt");
+  //  if (myFile) {
+  //    Serial.println("test.txt:");
+  //    
+  //    // read from the file until there's nothing else in it:
+  //    while (myFile.available()) {
+  //      Serial.write(myFile.read());
+  //  }
+  //    // close the file:
+  //    myFile.close();
+  //  } else {
+  //    // if the file didn't open, print an error:
+  //    Serial.println("error opening test.txt");
+  //  }
 }
 
 
@@ -92,37 +92,66 @@ void loop() {
     digitalWrite(ledPin, HIGH);
     Serial.println("Button pressed!");
 
+    if (SD.exists("test.txt")) {
+      // Read file
+      Serial.println("File exists");
+        myFile = SD.open("test.txt");
+        if (myFile) {
+          Serial.println("test.txt:");
+
+            // read from the file until there's nothing else in it:
+            while (myFile.available())
+            {
+              Serial.write(myFile.read());
+            }
+          // close the file:
+          myFile.close();
+          SD.remove("test.txt");
+        }
+        else {
+          // if the file didn't open, print an error:
+          Serial.println("error opening test.txt");
+        }
+    }
+    else {
+      Serial.println("File doesn't exist");
+    }
+
     // Write to file
     myFile = SD.open("test.txt", FILE_WRITE);
-  
+
     // if the file opened okay, write to it:
     if (myFile) {
       Serial.print("Writing to test.txt...");
       myFile.println(now.unixtime());
       myFile.close(); // close the file:
       Serial.println("done.");
-    } else {
+    }
+    else {
       // if the file didn't open, print an error:
       Serial.println("error opening test.txt");
     }
 
-     // re-open the file for reading:
-  myFile = SD.open("test.txt");
-  if (myFile) {
-    Serial.println("test.txt:");
-    
-    // read from the file until there's nothing else in it:
-    while (myFile.available()) {
-      Serial.write(myFile.read());
+    //SD.remove("test.txt");
+
+    // re-open the file for reading:
+   //  myFile = SD.open("test.txt");
+   //  if (myFile) {
+   //    Serial.println("test.txt:");
+   //    
+   //    // read from the file until there's nothing else in it:
+   //    while (myFile.available()) {
+   //      Serial.write(myFile.read());
+   //  }
+   //    // close the file:
+   //    myFile.close();
+   //  } else {
+   //    // if the file didn't open, print an error:
+   //    Serial.println("error opening test.txt");
+   //  }
+
   }
-    // close the file:
-    myFile.close();
-  } else {
-    // if the file didn't open, print an error:
-    Serial.println("error opening test.txt");
-  }
-    
-  } else {
+  else {
     // turn LED off:
     digitalWrite(ledPin, LOW);
   }
