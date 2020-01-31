a69f096e6ab77d5eb57cc1d1debe7dca952999ce
blaze21777 <stephen.411@hotmail.co.uk>
2020-01-31 09:16:08 +0000

    Modified SPI test.

--- Arduino-Projects/SPI_test/SPI_test.ino
+++ Arduino-Projects/SPI_test/SPI_test.ino
@@ -1,6 +1,7 @@
 char dataString[50] = {0};
 int a =0; 
 String data;
+int data2;
 int ledPin = 2;
 
 void setup() {
@@ -10,19 +11,19 @@ pinMode(ledPin, OUTPUT);
 }
   
 void loop() {
-    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-    delay(200);                       // wait for a second
-    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-    delay(200);
+//    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+//    delay(200);                       // wait for a second
+//    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+//    delay(200);
   if (Serial.available() > 0) {
-    data = Serial.read();
-      if (data == "hello"){
+//    data2 = Serial.read();
+//      if (data2 == 5){
         digitalWrite(ledPin, HIGH);   // turn the LED on (HIGH is the voltage level)
         delay(1000);                       // wait for a second
         digitalWrite(ledPin, LOW);    // turn the LED off by making the voltage LOW
         delay(1000);                      // wait for a second
-        } 
-        data = "baby";
+//        } 
+//        data2 = 0;
 //  a++;                          // a value increase every loop
 //  sprintf(dataString,"%02X",a); // convert a value to hexa 
 //  Serial.println(dataString);   // send the data
