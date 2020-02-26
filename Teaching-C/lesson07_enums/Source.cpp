f163a9e2292a83a09c473171223deb3b8f050617
blaze21777 <32124927+blaze21777@users.noreply.github.com>
2020-02-26 12:25:46 +0000

    Updated enums

--- Teaching-C/lesson07_enums/Source.cpp
+++ Teaching-C/lesson07_enums/Source.cpp
@@ -4,27 +4,51 @@ using namespace std;
 
 enum Choice
 {
-	EASY = 1,
-	MEDIUM = 2,
-	HARD = 3
+	FILL = 1,
+	HEAT = 2,
+	WASH_SLOW = 3,
+	EMPTY = 4,
+	RINSE = 5,
+	SPIN_FAST = 6,
+	SPIN_SLOW = 7,
+	DRY = 8,
+	COMPLETE = 9
 };
 int main() {
 	int i = -1;
 
 	// ...<present the user with a menu>...
-	cout << "Please type a number between 1 and 3 \n";
+	cout << "Please type a number between 1 and 9 \n";
 	cin >> i;
 
 	switch (i)
 	{
-	case EASY:
-		cout << "Easy\n";
+	case FILL:
+		cout << "FILL\n";
 		break;
-	case MEDIUM:
-		cout << "Medium\n";
+	case HEAT:
+		cout << "HEAT\n";
 		break;
-	case HARD:
-		cout << "Hard\n";
+	case WASH_SLOW:
+		cout << "WASH_SLOW\n";
+		break;
+	case EMPTY:
+		cout << "EMPTY\n";
+		break;
+	case RINSE:
+		cout << "RINSE\n";
+		break;
+	case SPIN_FAST:
+		cout << "SPIN_FAST\n";
+		break;
+	case SPIN_SLOW:
+		cout << "SPIN_SLOW\n";
+		break;
+	case DRY:
+		cout << "DRY\n";
+		break;
+	case COMPLETE:
+		cout << "COMPLETE\n";
 		break;
 	default:
 		cout << "Invalid Selection\n";
