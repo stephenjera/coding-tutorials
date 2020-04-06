06c1181f034ef7bfc4f426bb15ea4a3cb21891bb
blaze21777 <32124927+blaze21777@users.noreply.github.com>
2020-04-06 18:57:41 +0100

    Working code for LCD

--- Raspberry-Pi/LCD_display/lcd_display.py
+++ Raspberry-Pi/LCD_display/lcd_display.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python
-
+from RPi import GPIO
+from RPLCD.gpio import CharLCD
 import time
-from RPLCD import CharLCD
-lcd = CharLCD(cols=16, rows=2) # pin_rs=37, pin_e=35, pins_data=[33, 31, 29, 23])
+#from RPLCD import CharLCD
 
-lcd.write_string(u'Hello world!')
-time.sleep(2)
+# Setup display
+lcd = CharLCD(cols=16, rows=2, pin_rs=37, pin_e=35,
+              pins_data=[40, 38, 36, 32, 33, 31, 29, 23],
+	      numbering_mode=GPIO.BOARD,
+	      dotsize=8,
+              charmap='A02',
+              auto_linebreaks=True)
+
+lcd.write_string('hello\r\n world')
+time.sleep(5)
 lcd.clear()
-
-
