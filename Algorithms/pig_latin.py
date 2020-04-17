75f78ccf63b3470dcd8ef0a016bdadad6a14077a
blaze21777 <stephen.411@hotmail.co.uk>
2020-04-18 00:04:26 +0100

    Added string to list function.

--- Algorithms/pig_latin.py
+++ Algorithms/pig_latin.py
@@ -11,6 +11,15 @@
 # Function definitions
 
 
+def string_to_list(input_string):
+    """Copy string to list"""
+    for x in range(len(input_string)):  # Range function count from 0, increments of 1
+        input_list = [str]  # List to store user input
+        input_list.append(input_string[x]),
+        # print(len(input_string)),  # Use comma to end loops
+    return input_string
+
+
 def pig_latin():
     """Convert word to pig latin, takes a string as input"""
     control = True
@@ -24,7 +33,11 @@ def pig_latin():
                 # Algorithm goes here
                 # Should only have letters at this point
                 # Stings immutable need to convert to lists
-
+                vowels = ('a', 'e', 'i', 'o', 'u')  # Removed to use lower() function, 'A', 'E', 'I', 'O', 'U')
+                if word.startswith(vowels):
+                    word
+                else:
+                    pass
             else:
                 word = input("Enter a word: ")  # Keep looping until only letters entered
         except TypeError:  # This shouldn't trigger as all inputs will be strings
@@ -35,5 +48,5 @@ def pig_latin():
 # Main code
 
 
-pig_latin()
-
+#pig_latin()
+print(string_to_list("test"))
