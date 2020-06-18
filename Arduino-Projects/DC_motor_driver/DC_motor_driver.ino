4faac313f6b872648e93988a47cce38120b378f3
blaze21777 <stephen.411@hotmail.co.uk>
2020-06-18 03:13:22 +0100

    Inital code test, error with turning off.

--- Arduino-Projects/DC_motor_driver/DC_motor_driver.ino
+++ Arduino-Projects/DC_motor_driver/DC_motor_driver.ino
@@ -1,9 +1,30 @@
-void setup() {
-  // put your setup code here, to run once:
+/*******************
+ * H bridge gude 
+ * 10 = rotate 
+ * 01 = rotate
+ * 00 or 11 = stop 
+ *******************/
 
+#define ENABLE 5
+// Used to control the H bridge 
+#define DIRA 6 
+#define DIRB 7
+
+void setup() {
+  pinMode(ENABLE,OUTPUT);
+  pinMode(DIRA, OUTPUT);
+  pinMode(DIRB, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  digitalWrite(ENABLE,HIGH); // enable on
+  
+  digitalWrite(DIRA,HIGH); //one way
+  digitalWrite(DIRB,LOW);
+  /* CURRENTLY IGNORES EVERYTHING BEYOND THIS POINT??
+  delay(500); // spin for 3 seconds 
+  digitalWrite(ENABLE,LOW);
+  digitalWrite(DIRA,HIGH); //stop
+  digitalWrite(DIRB,HIGH);*/
 }
