912039aeca55218b8a1ad761b5d2a989e65ea047
blaze21777 <stephen.411@hotmail.co.uk>
2020-07-09 11:29:11 +0100

    Added comments about register functionality.

--- Arduino-Projects/Timer_interript_leds/Timer_interript_leds.ino
+++ Arduino-Projects/Timer_interript_leds/Timer_interript_leds.ino
@@ -10,8 +10,14 @@ volatile byte state = LOW;
  
 void setup() {
    pinMode(ledPin, OUTPUT);
-   TIMSK2 = (TIMSK2 & B11111110) | 0x01;
-   TCCR2B = (TCCR2B & B11111000) | 0x07;
+   //TIMSK2 = TC2 Interrupt Mask Register
+   // Keep old values and set Timer/Counter2, Overflow Interrupt Enable (TOIE)
+   TIMSK2 = (TIMSK2 & B11111110) | 0x01; 
+   
+   // TCCR2B = TC2 Control Register B
+   // The three Clock Select bits select the clock source to be used by the Timer/Counter.
+   // Change the clock divisor (default 64)
+   TCCR2B = (TCCR2B & B11111000) | 0x07; // Clock divisor = 1024
 }
  
  
@@ -19,7 +25,7 @@ void loop() {
    digitalWrite(ledPin, state);
 }
  
- 
+// Interrupt service routine 
 ISR(TIMER2_OVF_vect){
-   state = !state;
+   state = !state; // Blink LED
 }
