c1bb5bd11c52a2f8c6f2bd96c0004968fe569f7a
blaze21777 <stephen.411@hotmail.co.uk>
2020-07-09 11:57:57 +0100

    Added led array.

--- Arduino-Projects/Timer_interript_leds/Timer_interript_leds.ino
+++ Arduino-Projects/Timer_interript_leds/Timer_interript_leds.ino
@@ -5,11 +5,16 @@
  */
 
 const int ledPin = 13;
+int ledArray[] = {2,3,4,5};
 volatile byte state = LOW;
- 
  
 void setup() {
    pinMode(ledPin, OUTPUT);
+   // Initalise ledArray as outputs
+   // int = 4 bytes thus divide by 4
+   for (int i = 0; i < sizeof(ledArray)/4; i++) {
+    pinMode(ledArray[i], OUTPUT);
+  } 
    //TIMSK2 = TC2 Interrupt Mask Register
    // Keep old values and set Timer/Counter2, Overflow Interrupt Enable (TOIE)
    TIMSK2 = (TIMSK2 & B11111110) | 0x01; 
@@ -22,7 +27,15 @@ void setup() {
  
  
 void loop() {
+  //  COULD ADD A FUNCTION TO REMOVE FOR LOOP REPEAT
    digitalWrite(ledPin, state);
+   for (int i = 0; i < sizeof(ledArray)/4; i++) {
+    digitalWrite(ledArray[i])
+  } 
+   for (int i = 0; i < sizeof(ledArray)/4; i++) {
+    digitalWrite(ledArray[i])
+  } 
+   
 }
  
 // Interrupt service routine 
