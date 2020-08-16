3feab401f2b9211ec28e34a398ec33c3dbdac801
blaze21777 <stephen.411@hotmail.co.uk>
2020-08-16 08:08:31 +0100

    Setting up GLEW to use modern openGL

--- OpenGL/OpenGL/src/main.cpp
+++ OpenGL/OpenGL/src/main.cpp
@@ -1,4 +1,7 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+#include <iostream>
 
 int main(void)
 {
@@ -7,6 +10,8 @@ int main(void)
 	/* Initialize the library */
 	if (!glfwInit())
 		return -1;
+
+	
 
 	/* Create a windowed mode window and its OpenGL context */
 	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
@@ -18,6 +23,11 @@ int main(void)
 
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
+
+	if (glewInit() != GLEW_OK)
+		std::cout << "Error" << std::endl;
+
+	std::cout << glGetString(GL_VERSION);
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
