b89fe2cf9549067c815bce26263a5edee824c53c
blaze21777 <stephen.411@hotmail.co.uk>
2020-09-24 06:09:37 +0100

    Converting inputs to batch

--- Neural Networks/NN_from_scratch02.py
+++ Neural Networks/NN_from_scratch02.py
@@ -2,14 +2,26 @@ import numpy as np
 
 # Neuron example with random numbers
 # Every input has its own weight
-inputs = [1.1, 4, 6, 2]  # Can be a value from the input layer (tracked values) or other neurons
+inputs = [[1.1, 4, 6, 2],  # Can be a value from the input layer (tracked values) or other neurons
+          [2.1, 4, 3, -4],
+          [3.3, 2, 1, 2]]
+
+# Weights and biases do not change with batch inputs
 weights = [[3, 4.6, 4, 4],
-            [0.5, -0.91, 0.28, -0.5],
-            [-0.26, -0.27, 0.17, 0.87]]
+           [0.5, -0.91, 0.28, -0.5],
+           [-0.26, -0.27, 0.17, 0.87]]
 biases = [2, 3, 4]
 
+weights2 = [[3, 4.6, 4],
+           [0.5, -0.91, 0.28],
+           [-0.26, -0.27, 0.17]]
+biases2 = [2, 3, 4]
+
 # Order of weights and inputs matters
-output = np.dot(weights, inputs) + biases
-print(output)
+layer01_outputs = np.dot(inputs, np.array(weights).T) + biases
+#  Adding a second layer od neurons
+layer02_outputs = np.dot(layer01_outputs, np.array(weights2).T) + biases2
+
+print(layer02_outputs)
 
 
