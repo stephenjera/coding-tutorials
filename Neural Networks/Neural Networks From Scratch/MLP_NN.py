48dbb546ca22efbd2a2fe2ee5e1ab7ca75c81e69
blaze21777 <stephen.411@hotmail.co.uk>
2020-10-04 06:31:03 +0100

    Fixed back propagation

--- Neural Networks/Neural Networks From Scratch/MLP_NN.py
+++ Neural Networks/Neural Networks From Scratch/MLP_NN.py
@@ -16,6 +16,7 @@ class MLP:
            :param list num_hidden_layers: Each entry is the number of neurons in a layer,total is size of array
            :param int num_outputs: Number of outputs to neural net
         """
+        # Save parameters in class instance
         self.num_inputs = num_inputs
         self.num_hidden_layers = num_hidden_layers
         self.num_outputs = num_outputs
@@ -34,65 +35,77 @@ class MLP:
             # Number of rows = current layer
             # Number of columns = number of neurons in next layer
             w = np.random.rand(layers[i], layers[i+1])
-            print("weight matrix: ", w)
-            self.weights.append(w)  # Add newly created data to weights list (creates list of arrays)
+            print("weight matrix{}: {}".format(i, w))
+            # Creates weight matrices
+            self.weights.append(w)  # Add newly created data to weights list
         print("\n")
 
+        # Create a place to save activations
         activations = []
         for i in range(len(layers)):
-            a = np.zeros(layers[i])
+            a = np.zeros(layers[i])  # Number of zeroes = number of neurons in layer
             activations.append(a)
         self.activations = activations
 
+        # Create a place to save derivatives
+        # dE/dW, derivative of error with respect to weight
+        # Weight matrices are in-between layers so one less total layers
         derivatives = []
         for i in range(len(layers)-1):
-            d = np.zeros((layers[i], layers[i+1]))
+            # Rows = neurons in current layer, columns = neurons in next layer
+            d = np.zeros((layers[i], layers[i+1]))  # Zeros has multiple parameters so parenthesis needed
             derivatives.append(d)
         self.derivatives = derivatives
 
     def forward_propagation(self, inputs):
         """Computes the forward propagation of the network based on inputs
-        :param array inputs: inputs signals
-        :returns array activations: output values
+        :param (ndarray) inputs: inputs signals
+        :returns ndarray activations: output values
         """
-        # The first layer number of activation = number of inputs
+        # The input layer activation is just the input itself
         activations = inputs
         self.activations[0] = inputs
+
+        # Iterate through the network layers
         for i, w in enumerate(self.weights):
             # Calculate net inputs
-            # Net inputs are activation of the previous layer multiplied by the weights
+            # Net inputs are activations of the previous layer multiplied by the weights
             net_inputs = np.dot(activations, w)
 
             # Apply sigmoid activation function
             activations = self._sigmoid(net_inputs)
-            print("Activations: ", activations)
+            # print("Activations: ", activations)
+            # Activation(a) of layer 3 = sigmoid(h_3)  (h = output)
+            # h_3 = a_3 * W_2, the activation is for next layer
             self.activations[i+1] = activations
         print("\n")
-        return activations
+        return activations  # Return output layer activations
 
-    def back_propagation(self, error, verbose=False):
+    def back_propagation(self, error_nn, verbose=False):
+        # Error = expected output(y) - prediction
+        # Error = (y - a_[i+1])
+        # a = activations, h = output, s' = derivative
         # dE/dW_i = (y - a_[i+1]) s'(h_[i+1])) a_i
         # s'(h_[i+1]) = s(h_[i+1])(1 - s(h_[i+1])
         # s(h_[i+1]) = a_[i+1]
-
         # dE/dW_[i-1] = (y - a_[i+1] s'(h_[i+1])) w_i s'(h_i) a_[i-1]
 
         for i in reversed(range(len(self.derivatives))):
             activations = self.activations[i+1]
 
-            delta = error * self._sigmoid_derivative(activations)  # ndarray([0.1, 0.2]) --> ndarray([[0.1, 0.2]])
+            delta = error_nn * self._sigmoid_derivative(activations)  # ndarray([0.1, 0.2]) --> ndarray([[0.1, 0.2]])
             delta_reshaped = delta.reshape(delta.shape[0], -1).T
 
             current_activations = self.activations[i]  # ndarray([0.1, 0.2]) --> ndarray([[0.1], [0.2]])
             # change activations to column vector
             current_activations_reshaped = current_activations.reshape(current_activations.shape[0], -1)
 
-            self.derivatives[i] = np.dot(current_activations_reshaped, delta)
-            error = np.dot(delta_reshaped, self.weights[i].T)
+            self.derivatives[i] = np.dot(current_activations_reshaped, delta_reshaped)
+            error_nn = np.dot(delta, self.weights[i].T)
 
             if verbose:
                 print("Derivatives for W{}: {}".format(i, self.derivatives[i]))
-        return error
+        return error_nn
 
     @staticmethod
     def _sigmoid_derivative(x):
@@ -101,8 +114,8 @@ class MLP:
     @staticmethod
     def _sigmoid(x):
         """Sigmoid activation function
-        :param float x: Value to be processed
-        :returns float y: Output
+        :param x: Value to be processed
+        :returns y: Output
         """
         y = 1 / (1 + np.exp(-x))
         return y
