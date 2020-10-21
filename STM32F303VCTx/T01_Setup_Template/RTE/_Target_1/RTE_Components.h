9d95954d083c8006dbbcd21cd2a647c4889d4fbd
blaze21777 <stephen.411@hotmail.co.uk>
2020-10-21 06:30:44 +0100

    Initial template that compiles

---
+++ STM32F303VCTx/T01_Setup_Template/RTE/_Target_1/RTE_Components.h
@@ -0,0 +1,27 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Setup_Template' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f3xx.h"
+
+/*  Keil.Standalone::Device:STM32Cube LL:GPIO:1.9.0 */
+#define RTE_DEVICE_LL_GPIO
+/*  Keil.Standalone::Device:STM32Cube LL:RCC:1.9.0 */
+#define RTE_DEVICE_LL_RCC
+/*  Keil.Standalone::Device:Startup:1.0.0 */
+#define RTE_DEVICE_STARTUP_STM32F3XX    /* Device Startup for STM32F3 */
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ STM32F303VCTx/T01_Setup_Template/main.c
@@ -0,0 +1,6 @@
+#include "stm32f3xx.h"
+
+int main(void)
+{
+	while(1);
+}
