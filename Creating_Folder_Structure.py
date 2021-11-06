04fff4617c1720d0e465b85f72486485a1028d0f
blaze21777 <stephen.411@hotmail.co.uk>
2021-11-06 21:30:05 +0000

    Creating folder structures minus error handling

---
+++ Creating_Folder_Structure.py
@@ -0,0 +1,64 @@
+import os
+
+notes_to_frequency = {
+  "D4": 293.66,
+  "D#4": 311.13,
+  "E4": 329.63,
+  "F4": 349.23,
+  "F#4": 369.99,
+  "G4": 392.00,
+  "G#4": 415.30,
+  "A4": 440.00,
+  "A#4": 466.16,
+  "B4": 493.88,
+  "C5": 523.25,
+  "C#5": 554.37,
+  "D5": 587.33,
+  "D#5": 622.25,
+  "E5": 659.25,
+  "F5": 698.46,
+  "F#5": 739.99,
+  "G5": 783.99,
+  "G#5": 830.61,
+  "A5": 880.00,
+  "A#5": 932.33,
+  "B5": 987.77,
+  "C6": 1046.50,
+  "C#6": 1108.73,
+  "D6": 1174.66,
+  "D#6": 1244.51,
+  "E6": 1318.51,
+}
+
+
+
+directory = "Simulated_Dataset"
+
+# Parent Directory path
+parent_dir = "working_with_files"
+
+# Path
+path = os.path.join(parent_dir, directory)
+
+# Create the directory
+is_dir = bool(os.path.isdir(directory))
+print(path)
+print(is_dir)
+# TODO: fix error checking
+if not is_dir:
+    os.mkdir(path)
+    print("Directory '% s' created" % directory)
+else:
+    print("Already a directory")
+
+for key, value in notes_to_frequency.items():
+    # create notes directory
+    note_path = os.path.join(path, key)
+    is_dir = os.path.isfile(note_path)
+    print(is_dir)
+    if not is_dir:
+        os.mkdir(note_path)
+        print("Directory '% s' created" % key)
+    else:
+        print("Already a directory")
+
