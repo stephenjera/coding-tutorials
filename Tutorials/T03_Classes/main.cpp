58ebe111e61db9629b1419795dd1229b04087c0f
blaze21777 <stephen.411@hotmail.co.uk>
2022-02-27 16:28:48 +0000

    Working with abstract classes

---
+++ Tutorials/T03_Classes/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "classes.h"
+using std::cout;
+using std::endl;
+
+int main(){
+    Car honda("Honda");
+
+    /*
+    auto test = honda.getModel();
+    cout << test << endl;
+    test = lorry.getModel();
+    honda.move();
+    lorry.move();
+    */
+}
