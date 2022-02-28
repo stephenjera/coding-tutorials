078dbe0596973c6e2cfaa85193c3961d87571b69
blaze21777 <stephen.411@hotmail.co.uk>
2022-02-28 15:59:33 +0000

    Fixed speling

--- Tutorials/T01_Pointers/T01_Pointers/main.cpp
+++ Tutorials/T01_Pointers/T01_Pointers/main.cpp
@@ -8,8 +8,8 @@ void AddNumbers(int, int&, int*);
 int main()
 {
 	char* ChPoint; // Declare pointer to hold address
-	char Ch; // Declare varaible 
-	ChPoint = &Ch; // Assign the address of ch to pointer varaible 
+	char Ch; // Declare variable 
+	ChPoint = &Ch; // Assign the address of ch to pointer variable 
 	Ch = 'A'; // Assign 'A' to Ch variable 
 	cout << *ChPoint << endl; // Output the contents pointed to by the address
 	*ChPoint = 'B';
@@ -19,7 +19,7 @@ int main()
 
 	// Creating an integer pointer 
 	int* p; // Declare pointer variable 
-	int Integer; // Declare integer varaible
+	int Integer; // Declare integer variable
 	p = &Integer; // Assign p the address of integer 
 	*p = 12; // Assign 12 to 
 
@@ -28,7 +28,7 @@ int main()
 	cout << *p << endl << endl;;
 
 	// Playing with pointers 
-	int Integer1 = 4, Integer2 = 5, Integer3 = 6; // Declare and intials 3 interger 
+	int Integer1 = 4, Integer2 = 5, Integer3 = 6; // Declare and initialise 3 interger 
 	int* IntegerPointer1; // Create two integer pointers 
 	int* IntegerPointer2;
 
@@ -43,7 +43,7 @@ int main()
 	IntegerPointer1 = &Integer3; // Assign address to pointer
 	*IntegerPointer2 = Integer1 + Integer3; // Assign sum to Integer2
 	cout << *IntegerPointer1 * *IntegerPointer2 << endl; // Print product of Integer3 and Interger2
-	cout << Integer1 << " " << Integer2 << " " << Integer3 << " " << endl; //Print values in integer varaibles 
+	cout << Integer1 << " " << Integer2 << " " << Integer3 << " " << endl; //Print values in integer variables 
 
 	// Using AddNumbers function
 	int Num1, Num2, Num3;
