c9a423febed81363013f0f9dc901094b41ee5dc1
blaze21777 <stephen.411@hotmail.co.uk>
2022-03-02 16:33:58 +0000

    Added function to increase note generation

--- Neural Networks/Audio/Guitar Simulation/Audio_Data_From_Matlab.py
+++ Neural Networks/Audio/Guitar Simulation/Audio_Data_From_Matlab.py
@@ -1,26 +1,31 @@
 """
 This code uses a Matlab file to create simulated audio data for musical notes
+
+Every note will have: pluck position
 """
+
+# TODO modify create wav to include time shifting
 
 import matlab.engine
 import os
-import random
+import numpy as np
 from Notes_to_Frequency import notes_to_frequency
 
-directory = "Simulated_Dataset_Matlab"
+directory = "Simulated_Dataset_Matlab_Test"  # name of directory to create
+notes_num = 4  # number of notes to create
+# random.seed(20)
 
-random.seed(20)
 
-
-def create_wav(filename, freq, pluck_position=0.9):
+def create_wav(filename, freq, pluck_position=0.9, excitation_signal='excite-picked-nodamp.wav'):
     """
     Creates a wav file for the specified frequency
             :param filename: name of note
             :param freq: note frequency
             :param pluck_position: simulate pluck position on guitar
+            :param excitation_signal: signal used to create notes
     """
     eng = matlab.engine.start_matlab()  # start matlab engine
-    e = eng.audioread('excite-picked-nodamp.wav')
+    e = eng.audioread(excitation_signal)
     e = eng.transpose(e)
 
     fs = eng.double(44100)  # sample rate
@@ -37,6 +42,31 @@ def create_wav(filename, freq, pluck_position=0.9):
     eng.audiowrite(filename, l, fs, nargout=0)
 
 
+def play_style(path, note_name, excitation, pluck_pos):
+    """
+    create notes with different play styles
+    :param path: place to create file
+    :param note_name: name of the note to create
+    :param excitation: picked or plucked excitation signal
+    :param pluck_pos: position to pluck
+    """
+    pluck_pos = float(pluck_pos)  # convert from numpy float
+    pluck_int = int(pluck_pos*10)  # remove decimal from pluck position
+    pluck_str = str(pluck_int)
+    if excitation == "excite-picked-nodamp.wav":
+        style = "Picked"
+    else:
+        style = "Plucked"
+    for i in range(notes_num):
+        freq_shift = i - (notes_num / 2)
+        new_freq = value + freq_shift
+        print("New frequency: ", new_freq)
+        # unmodified note will be i == notes_num/2
+        created_path = path + "\\" + note_name + "_" + str(i) + "_" + style + "_" + pluck_str + ".wav"
+        create_wav(filename=created_path, freq=new_freq, excitation_signal=excitation, pluck_position=pluck_pos)
+        print("Created: ", created_path)
+
+
 if __name__ == "__main__":
     # create the directory
     if not os.path.exists(directory):
@@ -51,16 +81,32 @@ if __name__ == "__main__":
         # if path does not exist create it
         if not os.path.exists(note_path):
             os.mkdir(note_path)
-            # generate correct pitch signal
-            create_wav(filename=note_path + "\\" + key + ".wav", freq=value)  # ".wav" required for audiowrite
-            print("Created: " + note_path + "\\" + key + ".wav")
-            for j in range(10):
-                # amplitude_random = random.randint(5, 20)
-                freq_shift = random.uniform(-10, 10)  # random float to change frequency
+            # Loop through for all pluck positions
+            for x in np.arange(0.1, 1, 0.3):
+                # create frequency shifted notes for picked
+                play_style(path=note_path, note_name=key, excitation="excite-picked-nodamp.wav", pluck_pos=x)
+                # create frequency shifted notes for plucked
+                play_style(path=note_path, note_name=key, excitation="excite-plucked-nodamp.wav", pluck_pos=x)
+
+            """
+            for j in range(notes_num):
+                freq_shift = j - (notes_num/2)
                 new_freq = value + freq_shift
-                # append a number up to 10 to identify modified data
-                create_wav(filename=note_path + "\\" + key + str(j) + ".wav", freq=new_freq)
-                print("Created: " + note_path + "\\" + key + str(j) + ".wav")
+                print("New frequency: ", new_freq)
+                # unmodified note will be j == notes_num/2
+                create_wav(filename=note_path + "\\" + key + "_" + str(j) + "_Picked" + ".wav", freq=new_freq)
+                print("Created: " + note_path + "\\" + key + "_" + str(j) + "_Picked" + ".wav")
+
+            # frequency shift loop for excite-plucked-nodamp.wav
+            for k in range(notes_num):
+                freq_shift = k - (notes_num/2)
+                new_freq = value + freq_shift
+                print("New frequency: ", new_freq)
+                # unmodified note will be j == notes_num/2
+                create_wav(filename=note_path + "\\" + key + "_" + str(k) + "Plucked" + ".wav", freq=new_freq)
+                print("Created: " + note_path + "\\" + key + "_" + str(k) + "Plucked" + ".wav")
+"""
             print("Directory '% s' created" % key)
+
         else:
             print("Already a directory")
