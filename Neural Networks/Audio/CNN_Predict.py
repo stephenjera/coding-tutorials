9e5ebf2eeda2e8147b18e007906d2a4f4b5004fe
blaze21777 <stephen.411@hotmail.co.uk>
2022-03-22 22:31:23 +0000

    Added updated prediction metrics and predict function now prints predictions dataframe

--- Neural Networks/Audio/CNN_Predict.py
+++ Neural Networks/Audio/CNN_Predict.py
@@ -7,18 +7,22 @@ expected index, the model must be provided with the correct data.
 # TODO calculate confusion matrix metrics
 
 import numpy as np
-from sklearn.metrics import confusion_matrix, accuracy_score, \
+import pandas as pd
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, accuracy_score, \
     precision_score, recall_score, f1_score, classification_report
 import seaborn as sns
 from tensorflow.keras.models import load_model
 import matplotlib.pyplot as plt
 from CNN import load_data
 from CNN import predict
+from Notes_to_Frequency import notes_to_frequency
+from Notes_to_Frequency import notes_to_frequency_6
 
+DATASET_PATH = "Dataset_JSON_Files/Only_A5_Recorded_2.json"  # data used for predictions
+MODEL_PATH = "CNN_Model_Files/CNN_Model_Simulated_Dataset_Matlab_Extended.h5"
 
-DATASET_PATH = "Dataset_JSON_Files/Only_A4_Recorded_Trimmed3.json"  # data used for predictions
-MODEL_PATH = "CNN_Model_Files/CNN_Model_Matlab_Hybrid3.h5"
-
+LABELS = notes_to_frequency_6.keys()
+PLOT_TITLE = "Simulated Dataset Extended "  # Dataset name to be used in graph titles
 
 def prepare_data(dataset):
     # load dataset
@@ -46,13 +50,19 @@ if __name__ == "__main__":
     # make prediction on a sample
     predicted_note = []
     predicted_index = []
+    prediction = pd.DataFrame(columns=LABELS)
+    print(LABELS)
+    #prediction.columns = list(LABELS)
+
     for i in range(len(X)):
-        note, index = predict(model, X[i], y[i])
+        note, index, pred = predict(model, X[i], y[i])
         predicted_note.append(note)
         predicted_index.append(index)
+        prediction.loc[len(prediction.index)] = pred[0]
 
     #print(y)
     print(predicted_index)
+    print(prediction)
     cm = confusion_matrix(y, predicted_index)
     # tn, fp, fn, tp = confusion_matrix(y, predicted_index).ravel()
     # print("tn: {} fp: {} fn: {} tp: {}".format(tn, fp, fn, tp))
@@ -60,12 +70,12 @@ if __name__ == "__main__":
     # calculate metrics
     report = classification_report(y, predicted_index, zero_division=0)
     accuracy = accuracy_score(y, predicted_index)
-    precision_macro = precision_score(y, predicted_index, average="macro")
-    precision_micro = precision_score(y, predicted_index, average="micro")
-    recall_macro = recall_score(y, predicted_index, average="macro")
-    recall_micro = recall_score(y, predicted_index, average="micro")
-    f1_score_macro = f1_score(y, predicted_index, average="macro")
-    f1_score_micro = f1_score(y, predicted_index, average="micro")
+    precision_macro = precision_score(y, predicted_index, average="macro", zero_division=0)
+    precision_micro = precision_score(y, predicted_index, average="micro", zero_division=0)
+    recall_macro = recall_score(y, predicted_index, average="macro", zero_division=0)
+    recall_micro = recall_score(y, predicted_index, average="micro", zero_division=0)
+    f1_score_macro = f1_score(y, predicted_index, average="macro", zero_division=0)
+    f1_score_micro = f1_score(y, predicted_index, average="micro", zero_division=0)
 
     # calculate metrics from confusion matrix
     true_pos = np.diag(cm) # true positives are the diagonal of cm
@@ -89,22 +99,20 @@ if __name__ == "__main__":
 
     #print("Specificity: ", specificity)
 
-
     # plot confusion matrix
-    sns.heatmap(cm, annot=True)
-    plt.title("Confusion matrix")
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm)
+    disp.plot()
+    plt.title(PLOT_TITLE + "Confusion Matrix")
     plt.xlabel('Predicted')
     plt.ylabel('True')
     plt.show()
-
-
 
     # plot graph
     xaxis = []
     xaxis.extend(range(0, len(X)))
     plt.scatter(xaxis, predicted_note)
-    plt.title("Predicted Note of OnlyA4Recorded using CNN_Model_Matlab_Test")
-    plt.xlabel('Sample')
+    plt.title("Predicted Note of OnlyA4Recorded using " + PLOT_TITLE)
+    plt.xlabel('Note Sample')
     plt.ylabel('Predicted Note')
     plt.show()
 
