2a2f839cbc3a7c82f28b0c6a9173c73f01d29123
blaze21777 <stephen.411@hotmail.co.uk>
2022-05-13 21:52:36 +0100

    Cleaned up code

--- Neural Networks/Audio/Guitar Simulation/Audio_Data_From_Matlab.py
+++ Neural Networks/Audio/Guitar Simulation/Audio_Data_From_Matlab.py
@@ -8,12 +8,12 @@ Every note will have: pluck position
 
 import matlab.engine
 import os
-import numpy as np
 from Notes_to_Frequency import notes_to_frequency
 
-directory = "Simulated_Dataset_Matlab_Test"  # name of directory to create
-notes_num = 4  # number of notes to create
-# random.seed(20)
+directory = "Simulated_Dataset_Matlab_12frets"  # name of directory to create
+# Pluck position is audable symtric from 0.5, e.g 0.7 = 0.3 (kind of)
+PLUCK_POSITIONS = [0, 0.5, 0.7, 0.9]  # listening to the notes these showed noticeble diferences
+DETUNE_FREQUENCIES = [-2, -1, 0, 1, 2]
 
 
 def create_wav(filename, freq, pluck_position=0.9, excitation_signal='excite-picked-nodamp.wav'):
@@ -39,7 +39,8 @@ def create_wav(filename, freq, pluck_position=0.9, excitation_signal='excite-pic
     p = eng.double(pluck_position)  # pluck position
     l = eng.kspluck(eng.double(freq), nd, fs, e, B, A, p)
 
-    eng.audiowrite(filename, l, fs, nargout=0)
+    # Bits per sample > 16 to prevent clipping
+    eng.audiowrite(filename, l, fs, 'BitsPerSample', 32, nargout=0)
 
 
 def play_style(path, note_name, excitation, pluck_pos):
@@ -57,12 +58,11 @@ def play_style(path, note_name, excitation, pluck_pos):
         style = "Picked"
     else:
         style = "Plucked"
-    for i in range(notes_num):
-        freq_shift = i - (notes_num / 2)
+    for freq_shift in DETUNE_FREQUENCIES:
+        #freq_shift = i - (notes_num / 2)  #TODO change so scaling is better
         new_freq = value + freq_shift
         print("New frequency: ", new_freq)
-        # unmodified note will be i == notes_num/2
-        created_path = path + "\\" + note_name + "_" + str(i) + "_" + style + "_" + pluck_str + ".wav"
+        created_path = path + "\\" + note_name + "_" + str(freq_shift) + "_" + style + "_" + pluck_str + ".wav"
         create_wav(filename=created_path, freq=new_freq, excitation_signal=excitation, pluck_position=pluck_pos)
         print("Created: ", created_path)
 
@@ -81,12 +81,12 @@ if __name__ == "__main__":
         # if path does not exist create it
         if not os.path.exists(note_path):
             os.mkdir(note_path)
-            # Loop through for all pluck positions
-            for x in np.arange(0.1, 1, 0.3):
+            # Loop through for all pluck positions from 0.1 to 1 in steps of 0.3
+            for pluck in PLUCK_POSITIONS:
                 # create frequency shifted notes for picked
-                play_style(path=note_path, note_name=key, excitation="excite-picked-nodamp.wav", pluck_pos=x)
+                play_style(path=note_path, note_name=key, excitation="excite-picked-nodamp.wav", pluck_pos=pluck)
                 # create frequency shifted notes for plucked
-                play_style(path=note_path, note_name=key, excitation="excite-plucked-nodamp.wav", pluck_pos=x)
+                play_style(path=note_path, note_name=key, excitation="excite-plucked-nodamp.wav", pluck_pos=pluck)
 
             """
             for j in range(notes_num):
