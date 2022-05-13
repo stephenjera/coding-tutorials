4ba5b605454210e2ca58ae9f8a1759f67f56c6d8
blaze21777 <stephen.411@hotmail.co.uk>
2022-05-13 10:02:58 +0100

    Classes tutorial

--- Tutorials/T03_Classes/main.cpp
+++ Tutorials/T03_Classes/main.cpp
@@ -5,12 +5,12 @@ using std::endl;
 
 int main(){
     Car honda("Honda");
-
-    /*
+    Truck lorry("lorry");
+    
     auto test = honda.getModel();
     cout << test << endl;
     test = lorry.getModel();
     honda.move();
     lorry.move();
-    */
+    
 }
