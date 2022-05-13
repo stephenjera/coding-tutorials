5e5a1a03ca4512baba81c3cfc6ac3a8d702773d5
blaze21777 <stephen.411@hotmail.co.uk>
2022-05-13 22:50:43 +0100

    Deleted file_test.py and replaced with jupyter notebook

--- Tutorials/T02_Files/file_test.py
+++
@@ -1,9 +0,0 @@
-file = open("test.txt", "w") # Open file for writing
-file.write("Hello World!")
-file.close
-
-file = open("test.txt", "r") # Open file for reading
-print(file.read())
-file.close()
-
-input() # stop console closing
