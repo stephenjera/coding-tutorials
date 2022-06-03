a68bb1cb724162a0b416d5d328ded3de4bee23d5
blaze21777 <stephen.411@hotmail.co.uk>
2022-06-03 22:29:45 +0100

    Added examples on WHERE and multiple search operators

--- Tutorials/T01_SQL_Store/T01_SQL_Store.sql
+++ Tutorials/T01_SQL_Store/T01_SQL_Store.sql
@@ -1,10 +1,51 @@
+-- Used to select the database to use when many are present 
 USE sql_store;
 
+
+-- Will casue a syntax error if statements order is changed 
+-- Choose the name of the field (column)
 SELECT 
 	first_name, 
     last_name,
     points,
-    points * 10 + 100 as increase
+    points * 10 + 100 AS 'discount factor'  -- AS is the alias statement  
+FROM customers  -- Choose table to use 
+WHERE customer_id > 3  -- Used to filter records (rows)
+ORDER BY last_name DESC;
+
+
+-- Return all the products 
+-- name 
+-- unit price
+-- new price (unit price * 1.1)
+SELECT 
+	name,
+    unit_price,
+    unit_price * 1.1 AS 'new price'
+FROM products;
+
+
+-- Using the where statement 
+SELECT * 
 FROM customers
-WHERE customer_id > 3
-ORDER BY last_name DESC
+-- Interates over all customers and returns those that match condition 
+WHERE state != 'VA';
+
+
+-- Get orders placed in 2019
+SELECT * 
+FROM orders 
+WHERE order_date >= '2019-01-01';
+
+
+-- Combining multiple search conditions when filtering data
+SELECT * 
+FROM customers
+-- AND operator is always evaluated first 
+WHERE birth_date >= '1990-01-01' OR points > 1000 AND state = 'VA';
+
+
+
+
+
+
