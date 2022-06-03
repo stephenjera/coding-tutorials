799f58aed20cea88e07a484fc9a69552fec22b74
blaze21777 <stephen.411@hotmail.co.uk>
2022-06-03 22:48:11 +0100

    Added example on using IN statement

--- Tutorials/T01_SQL_Store/T01_SQL_Store.sql
+++ Tutorials/T01_SQL_Store/T01_SQL_Store.sql
@@ -42,7 +42,16 @@ WHERE order_date >= '2019-01-01';
 SELECT * 
 FROM customers
 -- AND operator is always evaluated first 
-WHERE birth_date >= '1990-01-01' OR points > 1000 AND state = 'VA';
+WHERE NOT birth_date >= '1990-01-01' OR 
+(points > 1000 AND state = 'VA');
+
+
+-- From the order_items table, get the items 
+-- for order #6
+-- where the total price is greater than 30 
+SELECT *
+FROM order_items
+WHERE order_id = 6 AND (quantity * unit_price > 30)
 
 
 
