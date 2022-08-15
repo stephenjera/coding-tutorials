83ad98dc5c7306b63efcabf046b55c2ffffa1311
blaze21777 <stephen.411@hotmail.co.uk>
2022-08-15 07:29:53 +0100

    Modified to return multiple values from function and print array

--- C_Tutorials/T10_Using_Malloc/main.c
+++ C_Tutorials/T10_Using_Malloc/main.c
@@ -1,13 +1,37 @@
 // Write a function that returns every multiple of 3 between 0 and 100.
+#include <stdio.h>
+#include <stdlib.h>
 
-int multiples3(int* arr[], int size){
+// used to return multiple values from function
+typedef struct values{
+    int* pArray;
+    int size;
+} t_values;
+
+t_values multiples3(){
+    // malloc returns pointer to memory block
     int* arr = malloc(100*sizeof(int));
     int count = 0;
-    for(int i = 0; i =< 100; i++){
+    int i;
+    for(i = 0; i <= 100; i++){
         if(i % 3 == 0){
             arr[count] = i;
             count++;
         }
-    }
-    return arr;
+    };
+    // arr is a point so function returns a pointer
+    t_values val;
+    val.pArray = arr;
+    val.size = count;
+    return val;
+}
+
+int main(){
+    t_values newArr = multiples3();
+    //printf("size of newArr %d value: %d", newArr.size, newArr.pArray[2]);
+    // print arrary 
+    for(int i; i < newArr.size; i++){
+        printf("index: %d value: %d \n", i, newArr.pArray[i]);
+    } 
+    return 0;
 }
