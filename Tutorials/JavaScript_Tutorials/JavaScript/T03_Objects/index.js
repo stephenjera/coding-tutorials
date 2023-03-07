00b55a0a706efb52a142bbad248012379da83378
stephenjera <stephen.411@hotmail.co.uk>
2023-03-07 19:52:43 +0000

    JavaScript objects

---
+++ Tutorials/JavaScript_Tutorials/JavaScript/T03_Objects/index.js
@@ -0,0 +1,13 @@
+const person = {
+  firstName: 'John',
+  lastName: 'Doe',
+  id: 5566,
+  fullName: function () {
+    return this.firstName + ' ' + this.lastName
+  }
+}
+
+console.log(`First name: ${person.firstName}`)
+console.log(`Last name: ${person.lastName}`)
+console.log(`Full name: ${person.fullName()}`)
+console.log(`Full name function: ${person.fullName}`)
