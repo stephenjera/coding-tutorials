f843470fefc51e59b8eaff3aca4e051b8dca50a8
stephenjera <stephen.411@hotmail.co.uk>
2023-03-07 21:33:29 +0000

    Updated objects

--- Tutorials/JavaScript_Tutorials/JavaScript/T03_Objects/index.js
+++ Tutorials/JavaScript_Tutorials/JavaScript/T03_Objects/index.js
@@ -11,3 +11,11 @@ console.log(`First name: ${person.firstName}`)
 console.log(`Last name: ${person.lastName}`)
 console.log(`Full name: ${person.fullName()}`)
 console.log(`Full name function: ${person.fullName}`)
+
+let x = new String('John')
+let y = new String('John')
+
+console.log()
+console.log('Comparing JS objects always results in false')
+console.log(`(x == y): ${(x == y)}`)
+console.log(`(x === y): ${(x === y)}`)
