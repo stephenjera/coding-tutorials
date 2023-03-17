510ceeaf35139be3d37d60072d232eb48debea78
stephenjera <stephen.411@hotmail.co.uk>
2023-03-17 21:51:08 +0000

    Added async and await

--- Tutorials/JavaScript_Tutorials/JavaScript/T11_Asyc_Patterns/T02_Promises/index.js
+++ Tutorials/JavaScript_Tutorials/JavaScript/T11_Asyc_Patterns/T02_Promises/index.js
@@ -1,14 +1,38 @@
-function doAsyncWork (resolve, reject) {
-  // do work
+// A JavaScript Promise object contains both the producing code and calls to the consuming code:
+let myPromise = new Promise(function (resolve, reject) {
+  // "Producing Code" (May take some time)
+
+  resolve() // when successful
+  reject() // when error
+})
+
+//Promise.then() takes two arguments, a callback for success and another for failure.
+// "Consuming Code" (Must wait for a fulfilled Promise)
+myPromise.then(
+  function (value) {
+    /* code if successful */
+  },
+  function (error) {
+    /* code if some error */
+  }
+)
+
+//Call back vs promise
+setTimeout(function () {
+  myFunction('I love You !!!')
+}, 3000)
+
+function myFunction (value) {
+  console.log(value)
 }
 
-// using named function
-let myPromise = new Promise(doAsyncWork)
+// Promise
+let myPromise2 = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    resolve('I love You !!')
+  }, 3000)
+})
 
-// using anonymous function (arrow function)
-let anotherPromise = new Promise((resolve, reject) => {})
-
-methodThatReturnsPromise()
-  .then(data => console.log(data)) // called when resolved
-  .catch(error => console.log(error)) // called when rejected
-  .finally(() => console.log('All done')) // called when everything is done
+myPromise2.then(function (value) {
+  console.log(value)
+})
