ad00b03b70aa0a22fb6dfc39628407399bc132c0
stephenjera <stephen.411@hotmail.co.uk>
2023-04-01 10:06:17 +0100

    Removed secret key

--- Tutorials/Django/T01_Hello_World/hello_world/settings.py
+++ Tutorials/Django/T01_Hello_World/hello_world/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sho&25-262%3ed=899q&l^x9f@h84fsn3i$--^h*1tyt$8y6k8'
+load_dotenv()
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
