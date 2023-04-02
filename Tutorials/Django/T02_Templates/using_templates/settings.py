ce86cbf2c15f0d5b3511b81c0b1393d64c341ff0
stephenjera <stephen.411@hotmail.co.uk>
2023-04-02 18:24:54 +0100

    Added 404 error template

--- Tutorials/Django/T02_Templates/using_templates/settings.py
+++ Tutorials/Django/T02_Templates/using_templates/settings.py
@@ -26,9 +26,9 @@ load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
