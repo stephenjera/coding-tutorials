fc7ad709ad5ee61be4f31ddefca8a3621cfd0689
stephenjera <stephen.411@hotmail.co.uk>
2023-04-06 20:03:31 +0100

    Updated order by tutorial

--- Tutorials/Postgres/T03_Order_By/orderby.sql
+++ Tutorials/Postgres/T03_Order_By/orderby.sql
@@ -4,8 +4,14 @@ select order_id,
 from orders
 order by order_date asc;
 
--- order b customer name length
+-- order by customer name length
 select customer_name,
     length(customer_name) as len
 from orders
 order by len;
+
+-- order by post code, nulls first 
+select customer_name,
+    postal_code
+from orders
+order by postal_code nulls first;
