33893b6b69100c5a0d2611619b01d28ebeb2f4b9
stephenjera <stephen.411@hotmail.co.uk>
2023-04-06 19:54:23 +0100

    Updated select tutorial

--- Tutorials/Postgres/T01_Select/select.sql
+++ Tutorials/Postgres/T01_Select/select.sql
@@ -1,1 +1,14 @@
-select * from orders 
+-- get every column
+select *
+from orders;
+
+-- get unique categories 
+select distinct category
+from orders;
+
+-- concatenation
+select customer_name || ' ' || ship_date as combined
+from orders;
+
+-- aliasing 
+select 5 * 3 as multiplication;
