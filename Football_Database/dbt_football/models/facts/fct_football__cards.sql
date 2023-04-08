95e07966c5f251ff02665553f8f2fd3ac621568a
stephenjera <stephen.411@hotmail.co.uk>
2023-04-08 14:34:08 +0100

    Removed unused ctes and joins

--- Football_Database/dbt_football/models/facts/fct_football__cards.sql
+++ Football_Database/dbt_football/models/facts/fct_football__cards.sql
@@ -2,17 +2,12 @@ with
 
     -- import CTEs
     cards as (select * from {{ ref("stg_football__cards") }}),
-    players as (select * from {{ ref("stg_football__players") }}),
-    matches as (select * from {{ ref("stg_football__matches") }}),
-    clubs as (select * from {{ ref("stg_football__clubs") }}),
     colours as (select * from {{ ref("stg_football__colours") }}),
 
     -- logic CTEs
     final as (
         select card_id, player_id, match_id, colour, time_given
         from cards
-        join players using (player_id)
-        join matches using (match_id)
         join colours using (colour_id)
     )
 
