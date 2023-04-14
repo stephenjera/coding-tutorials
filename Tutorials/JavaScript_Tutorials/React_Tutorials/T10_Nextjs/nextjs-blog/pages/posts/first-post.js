4398e432bd1974597342dfc5aa54557051ab60c3
stephenjera <stephen.411@hotmail.co.uk>
2023-04-14 07:42:34 +0100

    Updated next tutorial

--- Tutorials/JavaScript_Tutorials/React_Tutorials/T10_Nextjs/nextjs-blog/pages/posts/first-post.js
+++ Tutorials/JavaScript_Tutorials/React_Tutorials/T10_Nextjs/nextjs-blog/pages/posts/first-post.js
@@ -1,10 +1,11 @@
 import Link from 'next/link'
 import Head from 'next/head'
 import Script from 'next/script'
+import Layout from '../../components/layout';
 
 export default function FirstPost () {
   return (
-    <>
+    <Layout>
       <Head>
         <title>First Post</title>
       </Head>
@@ -19,6 +20,6 @@ export default function FirstPost () {
       <h2>
         <Link href='/'>← Back to home</Link>
       </h2>
-    </>
+    </Layout>
   )
 }
