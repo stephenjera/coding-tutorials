320d65f955e495f552a228ca34ad117b94a2ca81
stephenjera <32124927+stephenjera@users.noreply.github.com>
2024-04-21 18:36:08 +0100

    Added more join examples

--- sql-tutorials/Postgres/T07_Joins/joins.sql
+++ sql-tutorials/Postgres/T07_Joins/joins.sql
@@ -1,14 +1,37 @@
--- Select all orders and the corresponding return information from the orders and returns tables
-select orders.*,
+-- Select all orders and the corresponding return information
+select 
+    orders.*,
     returns.returned
 from orders
     inner join returns on orders.order_id = returns.order_id;
 
 -- Select all people, orders, and returns in the South Asian region
-select people.*,
+select 
+    people.*,
     orders.*,
     returns.*
 from people
     inner join orders on people.region = orders.region
     inner join returns on orders.order_id = returns.order_id
-where people.region = 'Southern Asia';
+where people.region = 'southern asia';
+
+-- Select all orders and corresponding returns, even if there's no return information
+select 
+    orders.*,
+    returns.*
+from orders
+    left join returns on orders.order_id = returns.order_id;
+
+-- Select all returns and corresponding orders, even if there's no order information for the return
+select 
+    orders.*,
+    returns.*
+from orders
+    right join returns on orders.order_id = returns.order_id;
+
+-- Select all orders and all returns, regardless of matching records
+select
+    orders.*,
+    returns.*
+from orders
+    full join returns on orders.order_id = returns.order_id;
